348379c982e8e08d67ed20fd703792d01b790b11
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2020-11-10 20:09:39 +0100

    First commit

---
+++ src/main/java/BinaryFacade/Main.java
@@ -0,0 +1,10 @@
+package BinaryFacade;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("0011", "0011"));
+        System.out.println(bins.mult("0011", "0011"));
+
+    }
+}
